05548a7e04b47080681bd9184b847f829d58be6c
thesuryac <thesuryac@gmail.com>
2024-09-07 23:39:28 +0530

    finished display

--- client/src/components/DisplayPannel.jsx
+++ client/src/components/DisplayPannel.jsx
@@ -1,15 +1,16 @@
 const DisplayPannel = ({ currentFloor, direction, load }) => {
   return (
-    <div className="h-1/3 p-3 w-full flex justify-around items-center bg-indigo-800">
-      <div className="text-2xl h-5/6 w-1/5 flex flex-col justify-center items-center bg-indigo-400 p-8 rounded-md">
+    <div className="h-1/3 p-3 w-full flex justify-around items-center bg-indigo-400 rounded-tr-2xl rounded-tl-2xl">
+      <div className="text-2xl h-5/6 w-1/5 flex flex-col justify-center items-center bg-indigo-700 p-8 rounded-md text-white">
         <span>direction</span>
+
         <span>{direction}</span>
       </div>
-      <div className=" font-bold text-5xl h-5/6 w-2/5 flex flex-col justify-center items-center bg-indigo-400 p-8 rounded-md">
+      <div className=" font-bold text-5xl h-5/6 w-2/5 flex flex-col justify-center items-center bg-indigo-700 p-8 rounded-md text-white">
         <span>floor</span>
         <span>{currentFloor}</span>
       </div>
-      <div className="text-4xl flex flex-col h-5/6 w-1/5 justify-center items-center bg-indigo-400 p-8 rounded-md">
+      <div className="text-4xl flex flex-col h-5/6 w-1/5 justify-center items-center bg-indigo-700 p-8 rounded-md text-white">
         <span>load</span>
         <span>{load}</span>
       </div>
