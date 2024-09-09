afd966d8907efec316b47405c3b163a4f66bc138
thesuryac <thesuryac@gmail.com>
2024-09-09 09:53:28 +0530

    resolved confilts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,41 +5,6 @@ import InsideButtons from "./components/InsideButtons";
 import { response } from "../data/sample";
 
 const App = () => {
-	const [out, setOut] = useState(true);
-	const floors = [
-		true,
-		false,
-		true,
-		false,
-		true,
-		false,
-		true,
-		false,
-		true,
-		false,
-	];
-	const currentFloor = 4;
-	const direction = "up";
-	const load = 4;
-	return (
-		<div className="h-screen w-screen bg-indigo-300 flex justify-center items-center">
-			<div className="h-3/4 w-3/4 shadow-lg bg-indigo-500 rounded-2xl">
-				<DisplayPannel
-					currentFloor={currentFloor}
-					direction={direction}
-					load={load}
-				/>
-				<div className="w-full h-2/3 flex justify-center items-center">
-					{out ? (
-						<OutsideButton currentFloor={currentFloor} />
-					) : (
-						<InsideButtons floors={floors} />
-					)}
-				</div>
-			</div>
-		</div>
-	);
-=======
   const [out, setOut] = useState(false);
   const [floors, setFloors] = useState([
     true,
@@ -57,18 +22,18 @@ const App = () => {
   const direction = "up";
   const load = 4;
 
-  const handleFloorClick  =  (index) =>  {
-    setFloors(prevFloors => {
+  const handleFloorClick = (index) => {
+    setFloors((prevFloors) => {
       // Create a shallow copy of the array
       const modifiedResult = [...prevFloors];
-      
+
       // Modify the specific index
       modifiedResult[index] = !prevFloors[index];
-      
+
       // Return the updated array
       return modifiedResult;
-  });
-  }
+    });
+  };
 
   return (
     <div className="h-screen w-screen bg-indigo-300 flex justify-center items-center">
@@ -79,7 +44,14 @@ const App = () => {
           load={load}
         />
         <div className="w-full h-2/3 flex justify-center items-center">
-          {out ? <OutsideButton /> : <InsideButtons floors={floors} handleFloorClick={handleFloorClick} />}
+          {out ? (
+            <OutsideButton />
+          ) : (
+            <InsideButtons
+              floors={floors}
+              handleFloorClick={handleFloorClick}
+            />
+          )}
         </div>
       </div>
     </div>
